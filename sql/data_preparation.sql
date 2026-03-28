15578360021a4ef3910fdfaa696ffd419ce95d4a
Shimon Hans <shimonsimanjuntak81@gmail.com>
2026-03-28 14:20:00 +0700

    Prepare the Time-Series Data

---
+++ sql/data_preparation.sql
@@ -0,0 +1,14 @@
+# Step 1: Prepare the Time-Series Data
+
+CREATE OR REPLACE TABLE `crm_predictive_analytics.revenue_time_series` AS
+SELECT
+  -- 'ds' is the standard column name for the Date Stamp
+  CAST(close_date AS DATE) AS ds,
+  -- 'y' is the standard column name for the Value we want to predict
+  SUM(close_value) AS y
+FROM `crm_predictive_analytics.sales_pipeline`
+WHERE deal_stage = 'Won'
+  AND close_date IS NOT NULL
+GROUP BY 1
+ORDER BY 1;
+
